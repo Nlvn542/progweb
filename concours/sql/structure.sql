5ca293cf956de65da42f79b38a83fac188b23e66
Nolan Jabin <nolan.jabin@edu.esiee-it.fr>
2026-06-05 10:07:34 +0200

    creation de table

--- concours/sql/structure.sql
+++ concours/sql/structure.sql
@@ -1,1 +1,7 @@
 CREATE DATABASE structure DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+CREATE TABLE client (
+    nb_question INT AUTO_INCREMENT,
+    question VARCHAR(500),
+    PRIMARY KEY(nb_question)
+) ENGINE InnoDB;
