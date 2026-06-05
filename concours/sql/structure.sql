ca731b677e5d2e584972ed352942db17a2714b7b
Nolan Jabin <nolan.jabin@edu.esiee-it.fr>
2026-06-05 11:30:58 +0200

    ajout index.php

--- concours/sql/structure.sql
+++ concours/sql/structure.sql
@@ -9,16 +9,16 @@ CREATE TABLE question (
 ) ENGINE InnoDB;
 
 INSERT INTO (question, reponse, points) VALUES 
-    ("Les protéines sont-ils importants pour les muscles ?", 1 , 10)
-    ("Les glucides sont-ils importants pour un sportif ?", 1, 10)
-    ("Les lipides sont-ils important pour un sportif ?", 1, 10)
-    ("Est ce que le poulet est rihes en glucides ?", 0, 25)
+    ("Les protéines sont-ils importants pour les muscles ?", 1 , 10),
+    ("Les glucides sont-ils importants pour un sportif ?", 1, 10),
+    ("Les lipides sont-ils important pour un sportif ?", 1, 10),
+    ("Est ce que le poulet est rihes en glucides ?", 0, 25),
     ("Est ce que les oeufs sont riches en protéines ?", 1, 45)
-    ("Les protéines servent-elles à réparer et construire les muscles après le sport ?", 1, 50)
-    ("Les pates et le riz sont riches en glucides ?", 1, 50)
-    ("l'huile d'olives contient des lipides bon pour la santé ?", 1, 50)
-    ("Le chocolat noir est mauvais pour la santé ?", 0, 75)
-    ("Un sportif doit enlever totalement les lipides pour être performant", 0, 75)
-    ("Les bonbons et les fruit contiennent le même type de glucides à assimilation lente que les pâtes complêtes", 0, 100)
+    ("Les protéines servent-elles à réparer et construire les muscles après le sport ?", 1, 50),
+    ("Les pates et le riz sont riches en glucides ?", 1, 50),
+    ("l'huile d'olives contient des lipides bon pour la santé ?", 1, 50),
+    ("Le chocolat noir est mauvais pour la santé ?", 0, 75),
+    ("Un sportif doit enlever totalement les lipides pour être performant", 0, 75),
+    ("Les bonbons et les fruit contiennent le même type de glucides à assimilation lente que les pâtes complêtes", 0, 100);
 
 
