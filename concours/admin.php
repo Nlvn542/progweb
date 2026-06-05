d94dc83ad6906d4ae8f781075e82f4e482202013
Nolan Jabin <nolan.jabin@edu.esiee-it.fr>
2026-06-05 12:11:21 +0200

    ajout admin.php

---
+++ concours/admin.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Administration du Concours</title>
+</head>
+<body>
+    <h1>Bienvenue sur l'administration des gestion des Questions</h1>
+</body>
+</html>
