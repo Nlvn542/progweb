0d930c3d1e9c696e28dc1b2f1943ac1a21aa8749
Nolan Jabin <nolan.jabin@edu.esiee-it.fr>
2026-06-05 14:26:50 +0200

    question 13

--- concours/admin.php
+++ concours/admin.php
@@ -1,7 +1,17 @@
 <?php
 require_once 'outils.php' ;
-$bdd = connexionBdd();
+try {
+
+    $bdd = connexionBdd();
+    $requete = $bdd->query('SELECT nb_question, question, points, reponse FROM question');
+    $question= $requete->fetchALL();
+
+    Var_dump($question);
+} catch (exception $e) {
+    die("Erreur : ".$e->getMessage());
+}
 ?>
+
 
 <!DOCTYPE html>
 <html lang="fr">
@@ -11,5 +21,28 @@ $bdd = connexionBdd();
 </head>
 <body>
     <h1>Bienvenue sur l'administration des gestion des Questions</h1>
+
+    <table border="1">
+        <thead>
+            <tr>
+                <th>nb_question</th>
+                <th>Question</th>
+                <th>Réponse</th>
+                <th>Points</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($question as $q): ?>
+                <tr>
+                    <td><?php echo $q['nb_question']; ?></td>
+                    <td><?php echo $q['question']; ?></td>
+                    <td><?php echo $q['reponse']; ?></td>
+                    <td><?php echo $q['points']; ?> pts</td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
 </body>
 </html>
+
+
--- concours/sql/structure.sql
+++ concours/sql/structure.sql
@@ -8,12 +8,12 @@ CREATE TABLE question (
     PRIMARY KEY(nb_question)
 ) ENGINE InnoDB;
 
-INSERT INTO (question, reponse, points) VALUES 
+INSERT INTO question (question, reponse, points) VALUES 
     ("Les protéines sont-ils importants pour les muscles ?", 1 , 10),
     ("Les glucides sont-ils importants pour un sportif ?", 1, 10),
     ("Les lipides sont-ils important pour un sportif ?", 1, 10),
     ("Est ce que le poulet est rihes en glucides ?", 0, 25),
-    ("Est ce que les oeufs sont riches en protéines ?", 1, 45)
+    ("Est ce que les oeufs sont riches en protéines ?", 1, 45),
     ("Les protéines servent-elles à réparer et construire les muscles après le sport ?", 1, 50),
     ("Les pates et le riz sont riches en glucides ?", 1, 50),
     ("l'huile d'olives contient des lipides bon pour la santé ?", 1, 50),
